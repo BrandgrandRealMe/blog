f3453a103fa52380b2c8e65d54dc6ab3ab6e8315
BrandgrandRealMe <brandonthedev@proton.me>
2025-06-01 04:14:09 -0500

    Testing Somethign

--- cf-build.sh
+++ cf-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  git fetch --unshallow && hugo --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo -b $CF_PAGES_URL
+  BASE_OPT=""
+  [ -n "$BASE_URL" ] && BASE_OPT="-b $BASE_URL"
+  git fetch --unshallow && hugo $BASE_OPT --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
 else
-  git fetch --unshallow && hugo --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo -b $CF_PAGES_URL
+  BASE_OPT=""
+  [ -n "$CF_PAGES_URL" ] && BASE_OPT="-b $CF_PAGES_URL"
+  git fetch --unshallow && hugo $BASE_OPT --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
 fi
