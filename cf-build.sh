a534c9d507f5513c66fce6b4b14e3d7dc6c3cfe2
BrandgrandRealMe <brandonthedev@proton.me>
2025-06-01 04:11:05 -0500

    Help

--- cf-build.sh
+++ cf-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  git fetch --unshallow && hugo -b $BASE_URL --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
+  git fetch --unshallow && hugo --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo -b $CF_PAGES_URL
 else
-  git fetch --unshallow && hugo -b $CF_PAGES_URL --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo
+  git fetch --unshallow && hugo --gc --templateMetrics --templateMetricsHints --forceSyncStatic --enableGitInfo -b $CF_PAGES_URL
 fi
